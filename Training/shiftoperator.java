b593eb4473bb39f9ea0573aff507cf207d80e047
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-08-28 09:07:17 +0530

    a

---
+++ Training/shiftoperator.java
@@ -0,0 +1,11 @@
+public class shiftoperator
+{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        System.out.println("Performing left shift");
+        System.out.println(a<<b);
+        System.out.println("Performing left shift");
+        System.out.println(a>>b);
+    }
+}
