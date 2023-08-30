2fb60c3d52fa33384651bc44ef0697877613a5ac
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-08-30 12:27:38 +0530

    a

--- Training/Day8/Protected_Access.java
+++ Training/Day8/Protected_Access.java
@@ -1,5 +1,13 @@
 package Day8;
-
-public class Protected_Access {
+class Parent{
+    protected void display(){
+        System.out.println("Batch 2");
+    }
+}
+ class Protected_Access extends Parent{
+    public static void main(String[] args) {
+        Protected_Access p1 = new Protected_Access();
+        p1.display();
+    }
     
 }
