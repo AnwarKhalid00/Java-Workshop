4957fcda9348f5bfe8ff8254eddf218d3b7818ca
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-01 10:55:24 +0530

    a

--- Training/Day9/Factorial.java
+++ Training/Day9/Factorial.java
@@ -1,28 +1,22 @@
 package Day9;
 import java.util.*;
-import java.lang.*;
-    int num;
-    void input(){
-        Scanner sc = new Scanner(System.in):
-        int num = sc.nextInt();
-        return num ;
-    }
-}
+
 class Fact{
     int num;
     int input(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the numner:")
+        System.out.println("Enter the number:");
         int num = sc.nextInt();
         return num;
+
     }
     int fact;
     int fac(){
         fact = 1;
         int i = 1;
         while(i<=num){
-            fact *= i;
-            i += 1;
+            fact = fact*i;
+            i = i+1;
         }
         return fact;
     }   
