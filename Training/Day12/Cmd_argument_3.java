c49cce1ddc3ec7bd20846e568a5f9f2b2fcaddd9
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-04 10:26:28 +0530

    a

---
+++ Training/Day12/Cmd_argument_3.java
@@ -0,0 +1,12 @@
+package Day12;
+
+public class Cmd_argument_3 {
+    public static void main(String[] ab)
+    { 
+        for(int i = 0;i<ab.length;i++)
+        {
+             int n = Integer.parseInt(ab[i]);
+            System.out.println(n);
+        }
+    }
+}
