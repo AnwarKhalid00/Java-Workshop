3fe4557229c01c402d33a4c161efc56e2873fcd8
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-04 10:55:24 +0530

    a

---
+++ Training/Day12/Gar_bage_collector.java
@@ -0,0 +1,20 @@
+package Day12;
+class Cricketer{
+    Cricketer(){
+        System.out.println("Object is created!!!!");
+    }
+    protected void finalize(){
+        System.out.println("Object is destroyed!!!");
+    }
+}
+public class Gar_bage_collector {
+    public static void main(String[] args) {
+        Cricketer c1 = new Cricketer();
+        c1 = null;//unrefernced obj is created.
+        Cricketer c2 = new Cricketer();  
+        c1 = c2;
+        new Cricketer();
+        System.gc();//Garbage collector.             
+
+    }
+}
