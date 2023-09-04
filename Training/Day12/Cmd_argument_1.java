18e7c4ddafa8e25deb1bcd63db16fbb6c072172f
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-04 10:09:04 +0530

    a

---
+++ Training/Day12/Cmd_argument_1.java
@@ -0,0 +1,9 @@
+package Day12;
+
+public class Cmd_argument_1 {
+    public static void main(String[] args)
+    { 
+      System.out.println("Hello Batch 2!!!");
+      System.out.println(args[0]);
+    }
+}
