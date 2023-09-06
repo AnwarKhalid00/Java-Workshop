fcfe49045c6310e0ff27b3b132e3487631bdd8a8
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-06 09:34:28 +0530

    a

--- Training/Day13/Exception_TryCatchandFinally_Block.java
+++ Training/Day13/Exception_TryCatchandFinally_Block.java
@@ -1,16 +1,22 @@
 package Day13;
 
-public class Exception_TryCatchandFinally_Block {
-    public static void main(String[] args) {
+public class Exception_TryCatchandFinally_Block 
+{
+    public static void main(String[] args) 
+    {
         int n = 26;
-        try {
+        try 
+        {
             int z = n/0;
             System.out.println(z);
-        } catch (Exception ae) {
+        } 
+        catch (Exception ae)
+        {
             System.out.println("Catch Block!!");
             System.out.println(ae.getMessage());
         }
-        finally{
+        finally
+        {
             System.out.println("Finally Block!!");
         }
     }
