5419c1346a24c5bda3300de9adfbd9c4b87e3505
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-13 11:57:38 +0530

    a

--- Training/Day17/File_handling_bufferinput.java
+++ Training/Day17/File_handling_bufferinput.java
@@ -9,8 +9,8 @@ public class File_handling_bufferinput {
         FileInputStream fin = new FileInputStream("Batch2Fileout.txt");
         BufferedInputStream bin = new BufferedInputStream(fin);
         int i;
-        while((i = bin.read())!=-1);{
-            System.out.println((char)i);
+        while((i = bin.read())!=-1){
+            System.out.print((char)i);
         }
         bin.close();
         fin.close();
