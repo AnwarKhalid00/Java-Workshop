a52656a1ecfb06745365a2eb725edf8a7e9fd890
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-13 10:57:54 +0530

    a

--- Training/Day17/File_handling_input.java
+++ Training/Day17/File_handling_input.java
@@ -1,5 +1,20 @@
 package Day17;
-import java.oi.*;
+
+import java.io.*;
 public class File_handling_input {
-    
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("Batch2Fileout.txt");
+            int i = 0;
+            while((i = fin.read())!=-1){//we use while loop to print the whole string at once!!
+                System.out.print((char)i);//without using loop we will get only single character at a time!!
+            }
+            fin.close();
+            
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+    }
 }
