448545a14fc0ce5de23b236ddfbb0fcb4039bcb6
AnwarKhalid00 <134593104+AnwarKhalid00@users.noreply.github.com>
2023-09-15 11:07:40 +0530

    a

--- Training/Day19/String_Tokenizer_2.java
+++ Training/Day19/String_Tokenizer_2.java
@@ -2,7 +2,7 @@ package Day19;
 import java.util.*;
 public class String_Tokenizer_2 {
     public static void main(String[] args) {
-        StringTokenizer s = new StringTokenizer("Hi@Welcome@To@Java@Tokenizer@!!");
+        StringTokenizer s = new StringTokenizer("He@Welcome@To@Java@Tokenizer@!!");
         while(s.hasMoreTokens()){
             System.out.println(s.nextToken("@"));
         }
---
+++ Training/Day19/String_Tokenizer_3.java
@@ -0,0 +1,9 @@
+package Day19;
+import java.util.*;
+public class String_Tokenizer_3 {
+    public static void main(String[] args) {
+        StringTokenizer st = new StringTokenizer("Hello Friends Hi !!");
+        System.out.println(st.countTokens());
+    }
+    
+}
