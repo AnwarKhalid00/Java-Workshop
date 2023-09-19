2ebb8ee9a9270e1d49b93b10c378ae5b5726a0fd
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-19 11:43:52 +0530

    a

--- Training/Day21/Producer_consumer.java
+++ Training/Day21/Producer_consumer.java
@@ -1,29 +1,29 @@
 package Day21;
-//psuedo code for producer and consumer!!!
-//class Item{
-//    int o;
-//    boolean produced=false;
-//    public void synchronized produced(int x){
-//        if(produced){
-//            try{
-//                wait();
-//          }catch(Exception e){
-//
-//            }
-//            i = x;
-//            produced = true;
-//           notify();
-//        }
-//        consume(){
-//            if(!produced){
-//                try {
-//                    wait();
-//                } catch (Exception e) {
-//                    
-//                }
-//                produced = false;
-//                notify();
-//            }
-//        }
-//    }
-//}
+//psuedo code for producer and consumer??
+/*class Item{
+    int o;
+    boolean produced=false;
+    public void synchronized produced(int x){
+        if(produced){
+          try{
+                wait();
+         }catch(Exception e){
+
+           }
+            i = x;
+           produced = true;
+           notify();
+        }
+        consume(){
+           if(!produced){
+                try {
+                    wait();
+                } catch (Exception e) {
+                    
+               }                
+               produced = false;
+               notify();
+           }
+        }
+   }
+}*/
