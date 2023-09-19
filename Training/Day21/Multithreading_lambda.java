596062fdbf9fff15858f4de2371184b47b613536
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-19 10:12:20 +0530

    a

---
+++ Training/Day21/Multithreading_lambda.java
@@ -0,0 +1,36 @@
+package Day21;
+
+public class Multithreading_lambda {
+    public static void main(String[] args) 
+    {
+        Runnable obj1 = ()->
+        {
+             for(int i = 0;i<5;i++){
+                    System.out.println("Hi");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        
+                    }
+                }
+            
+        };
+        Runnable obj2 = ()->
+        {
+            for(int i = 0;i<5;i++){
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                        
+                    }
+                }
+            
+        };
+        Thread t1 = new Thread(obj1);
+        t1.start();
+        Thread t2 = new Thread(obj2);
+        t2.start();
+    }
+    
+}
