0715dd836f54e873663a6fe7b5a7fd239cc8a1d4
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-20 12:20:58 +0530

    a

---
+++ Training/Day22/Generic.java
@@ -0,0 +1,12 @@
+package Day22;
+import java.util.*;
+public class Generic {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        Integer s = list.get(0);
+        System.out.println(s);
+        System.out.println(list);
+    }
+}
--- Training/Day22/Wrapper_Unboxing.java
+++ Training/Day22/Wrapper_Unboxing.java
@@ -9,3 +9,4 @@ public class Wrapper_Unboxing {
     }
     
 }
+ 
