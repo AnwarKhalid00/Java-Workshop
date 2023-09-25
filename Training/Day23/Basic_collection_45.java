6145a834229608c67180b8577c60afd6e8e5e2d5
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-25 11:48:48 +0530

    a

--- Training/Day23/Basic_collection_45.java
+++ Training/Day23/Basic_collection_45.java
@@ -10,8 +10,12 @@ public class Basic_collection_45 {
         System.out.println("Elements: "+as);
         Collections.addAll(as,"HTML","CSS","JavaScript","DSA","DBMS","DS");
         System.out.println("Elements after addition using collection: "+as);
-        System.out.println("Index value: "+as.get(7));
-        as.set(4,"Algo");
+        System.out.println("Index value: "+as.get(7));//get method is used to get the elements from the list.
+        as.set(4,"Algo");//set method is used to update the elements in the list.
         System.out.println("Updated Elements: "+as);
+        as.remove("C");
+        System.out.println("Elements after removal: "+as); //remove method is used to remove the elements from the list. 
+        as.remove(3);
+        System.out.println("Elements after removal: "+as);
     }
 }
