e676e430a313d0cd7b31b9f1ab05950c5b818e46
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-25 10:01:25 +0530

    a

---
+++ Training/Day23/Basic_Collection_without_generics.java
@@ -0,0 +1,12 @@
+package Day23;
+import java.util.*;
+public class Basic_Collection_without_generics {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(10);//add method is used to add elements in the end of the list.
+        al.add("B2");
+        al.add(20.45f);
+        System.out.println("Elements: "+al);
+        
+    }
+}
