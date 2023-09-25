579a49397f788e20e4465aa1891de731e6b4b678
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-25 10:12:49 +0530

    a

---
+++ Training/Day23/Basic_Collection_with_integer.java
@@ -0,0 +1,17 @@
+package Day23;
+import java.util.*;
+public class Basic_Collection_with_integer {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(56);//add method is used to add elements in the end of the list.
+        al.add(89);
+        al.add(12);
+        al.add(34);
+        System.out.println("Elements: "+al);
+        Iterator it = al.iterator();
+        System.out.println("Iterating Elements: ");
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
