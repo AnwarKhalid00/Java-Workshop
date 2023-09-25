69b0dded5ec596f39c471d7d814c0a81116057c7
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-25 10:59:28 +0530

    a

---
+++ Training/Day23/Basic_Collection_4.java
@@ -0,0 +1,28 @@
+package Day23;
+import java.util.*;
+public class Basic_Collection_4 {
+    public static void main(String[] args) {
+        ArrayList li = new ArrayList();
+        li.add(10);//add method is used to add elements in the end of the list.
+        li.add("Niet");
+        li.add("Pyramid");
+        li.add(275);
+        li.add(456);
+        System.out.println("Without Generic: ");
+        for(Object s:li){
+            System.out.println(s+" ");
+        }
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(10);//add method is used to add elements in the end of the list.
+        al.add(227);
+        al.add(38);
+        al.add(409);
+        al.add(5000);
+        System.out.println("With Generic: ");
+        for(Integer s:al){
+            System.out.println(s+" ");
+        }
+
+
+    }
+}
---
+++ Training/Day23/Basic_Collection_removing_elements.java
@@ -0,0 +1,26 @@
+package Day23;
+import java.util.*;
+public class Basic_Collection_removing_elements {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Batch 2");//add method is used to add elements in the end of the list.
+        al.add("Khalid");
+        al.add("Mayank");
+        al.add("Rajeev");
+        al.add("Kashif");
+        System.out.println("Elements: "+al);
+        Iterator i = al.iterator();
+        String str = "";
+        while(i.hasNext()){
+            str = (String)i.next();
+            if(str.equals("Batch 2")){
+                i.remove();
+                System.out.println("After removing");
+                break;
+            }
+            
+        }
+        System.out.println("Elements: "+al);
+        
+    }
+}
