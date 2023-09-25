96955914570f1bcedd75bbf92457d39f50f72199
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-25 11:11:48 +0530

    a

---
+++ Training/Day23/Question.java
@@ -0,0 +1,17 @@
+//WAP to read floating values using ArrayList and display it for using for each loop??
+package Day23;
+import java.util.*;
+public class Question {
+    public static void main(String[] args) {
+        ArrayList<Float> al = new ArrayList<Float>();
+        al.add(10.0f);//add method is used to add elements in the end of the list.
+        al.add(227.0f);
+        al.add(38.0f);
+        al.add(409.0f);
+        al.add(5000.0f);
+        System.out.println("With Generic: ");
+        for(Float s:al){
+            System.out.println(s+" ");
+        }
+    }
+}
