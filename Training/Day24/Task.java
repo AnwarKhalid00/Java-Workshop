4e451477b8686e70c59afcd3203b8848ac180fcc
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-26 09:21:24 +0530

    a

---
+++ Training/Day24/Task.java
@@ -0,0 +1,23 @@
+package Day24;
+//write a program to print the maximum element from the arraylist.
+import java.util.*;
+public class Task {
+    public static void main(String[] args) {
+        ArrayList<Integer> ai = new ArrayList<Integer>();
+        ai.add(16);//add method is used to add elements in the end of the list.
+        ai.add(38);
+        ai.add(15);
+        ai.add(401);
+        ai.add(59);
+        ai.add(10);
+        ai.add(27);
+        System.out.println("Elements: "+ai);
+        int max = ai.get(0);
+        for(int i=0;i<ai.size();i++){
+            if(ai.get(i)>max){
+                max = ai.get(i);
+            }
+        }
+        System.out.println("Maximum element: "+max);
+    }
+}   
