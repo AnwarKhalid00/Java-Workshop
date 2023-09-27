c574b9d8edb25fa216c91f72fee4e945ed4ac958
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-27 12:15:08 +0530

    a

---
+++ Training/Day25/Priority_queue.java
@@ -0,0 +1,19 @@
+package Day25;
+import java.util.*;
+public class Priority_queue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        pq.offer(4);
+        pq.add(6);
+        pq.offer(5);
+        pq.add(1);
+        System.out.println("Priority Queue implementation: " + pq);
+        PriorityQueue<Integer> pq2 = new PriorityQueue<Integer>(Collections.reverseOrder());    
+        pq2.offer(4);
+        pq2.add(6);
+        pq2.offer(5);
+        pq2.add(1);
+        System.out.println("Priority Queue implementation: " + pq2);
+
+    }
+}
