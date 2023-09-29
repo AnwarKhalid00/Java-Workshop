d79bd6e0a5a6849f4d5021960441dffe45dc8865
AnwarKhalid00 <anwarkhalid069@gmail.com>
2023-09-29 09:50:28 +0530

    a

---
+++ Training/Day26/Basic_Test.java
@@ -0,0 +1,10 @@
+package Day26;
+
+public class Basic_Test {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Training/Day26/Basic_TestTest.java
@@ -0,0 +1,10 @@
+package Day26;
+
+import org.junit.Test;
+
+public class Basic_TestTest {
+    @Test
+    public void testAdd() {
+
+    }
+}
