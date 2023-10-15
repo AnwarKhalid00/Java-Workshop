33db6d828c61140a4295b4c6dc88b14b0d8aca6a
AnwarKhalid <134593104+AnwarKhalid00@users.noreply.github.com>
2023-10-15 14:42:51 +0530

    a

--- Training/Day4/breakstatement.java
+++ Training/Day4/breakstatement.java
@@ -4,7 +4,7 @@ public class breakstatement
 {
     public static void main(String[] args) 
     {
-        for(int i=1;i<=5;i++)
+        for(int i=0;i<=5;i++)
         {
             if(i==4)
             {
