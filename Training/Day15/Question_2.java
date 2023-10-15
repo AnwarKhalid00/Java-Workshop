479116168c2aafc4d2d97195fddee63f51b2e8de
AnwarKhalid <134593104+AnwarKhalid00@users.noreply.github.com>
2023-10-15 17:27:42 +0530

    a

--- Training/Day15/Question_2.java
+++ Training/Day15/Question_2.java
@@ -1,11 +1,19 @@
-package Day15;
 //WAP to print sum of even numbers using local inner class?
-import java.util.*;
-
-public class Question_2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        
+package Day15;
+class Local{
+    int sum = 0;
+    void show(){
+        class Local_nested{
+            void disp(){
+                for(int i = 0;i<=10;++i){
+                    if(i%2==0){
+                        sum+=i;
+                    }
+                }
+                System.out.println("Sum of even numbers:"+sum);
+            }
+        }
+        Local_nested l = new Local_nested();
+        l.disp();
     }
 }
